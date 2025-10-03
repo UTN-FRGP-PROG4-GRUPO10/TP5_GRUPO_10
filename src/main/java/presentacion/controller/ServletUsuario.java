bd058b4151a8b2e3864ee0825d65c918b11f31ee
Tomás Martínez <tomas.emart@gmail.com>
2025-10-03 00:35:54 -0300

    feat: add ServlerUsuario with doPost for basic login

---
+++ src/main/java/presentacion/controller/ServletUsuario.java
@@ -0,0 +1,42 @@
+package presentacion.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entidad.Usuario;
+import negocio.UsuarioNeg;
+import negocio.impl.UsuarioNegImpl;
+
+@WebServlet("/ServletUsuario")
+public class ServletUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	UsuarioNeg usuarioNeg = new UsuarioNegImpl();
+       
+    public ServletUsuario() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("btnLogin") != null) {
+			String usuario = request.getParameter("txtUsuario");
+			String contrasena = request.getParameter("txtContrasena");
+			Usuario u = usuarioNeg.obtenerLogin(usuario, contrasena);
+			
+			request.setAttribute("usuario", u);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/Login.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
--- src/main/webapp/Login.jsp
+++ src/main/webapp/Login.jsp
@@ -40,21 +40,35 @@
 </head>
 <body>
 	<div class="contenedor-login">
-<form>
+<form action="ServletUsuario" method="post">
 	<table>
 		<tr>
 
 			<td><label>Usuario:</label>
-			<input type="text"/></td>
+			<input type="text" name="txtUsuario" required/></td>
 			
 		</tr>
 		<tr>
 			<td><label>Contraseña:</label>
-				<input type="password"/></td>
+				<input type="password" name="txtContrasena" required/></td>
 		</tr>
 		<tr>
-			<td><input type="submit" value="Ingresar"/></td>
+			<td><input type="submit" value="Ingresar" name="btnLogin"/></td>
 		</tr>
+		<%
+			if(request.getParameter("btnLogin") != null){
+				if(request.getAttribute("usuario") != null) { %>
+					<tr>
+						<td><label style="color: green;">Sesión iniciada correctamente.</label></td>
+					</tr>
+				<% }
+				else { %>
+					<tr>
+						<td><label style="color: red;">Nombre de usuario o contraseña incorrectos.</label></td>
+					</tr>
+				<% }
+			}
+		%>
 	</table>
 </form>
 </body>
